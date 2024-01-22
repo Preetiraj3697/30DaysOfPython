67cdbbc2d2b3b92a4783ccd184b0daa91dae5263
Preeti Raj <satendra11kum@gmail.com>
2024-01-22 23:25:56 +0530

    Day 5 exercise level 2

--- Day5/List.py
+++ Day5/List.py
@@ -106,7 +106,7 @@ print(fruits)       # ['orange', 'mango', 'lemon']
 del fruits[1]     
 print(fruits)       # ['orange', 'lemon']
 del fruits
-print(fruits)       # This should give: NameError: name 'fruits' is not defined
+# print(fruits)       # This should give: NameError: name 'fruits' is not defined
 
 # clear
 fruits = ['banana', 'orange', 'mango', 'lemon']
@@ -300,4 +300,248 @@ full_stack.insert(redux_index + 2, 'SQL')
 
 print("Updated full stack technologies:", full_stack)
 
+# Exercise Level 2
 
+# 1. The following is a list of 10 students ages:
+ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+# - Sort the list and find the min and max age
+ages.sort()
+min_age = min(ages)
+max_age = max(ages)
+# - Add the min age and the max age again to the list
+ages.extend([min_age, max_age])
+# - Find the median age (one middle item or two middle items divided by two)
+middle_index = len(ages) // 2
+median_age = (ages[middle_index - 1] + ages[middle_index]) / 2 if len(ages) % 2 == 0 else ages[middle_index]
+
+# - Find the average age (sum of all items divided by their number )
+average_age = sum(ages) / len(ages)
+# - Find the range of the ages (max minus min)
+age_range = max_age - min_age
+# - Compare the value of (min - average) and (max - average), use _abs()_ method
+min_average_diff = abs(min_age - average_age)
+max_average_diff = abs(max_age - average_age)
+
+# 2. Find the middle country(ies) in the countries list
+countries = [
+  'Afghanistan',
+  'Albania',
+  'Algeria',
+  'Andorra',
+  'Angola',
+  'Antigua and Barbuda',
+  'Argentina',
+  'Armenia',
+  'Australia',
+  'Austria',
+  'Azerbaijan',
+  'Bahamas',
+  'Bahrain',
+  'Bangladesh',
+  'Barbados',
+  'Belarus',
+  'Belgium',
+  'Belize',
+  'Benin',
+  'Bhutan',
+  'Bolivia',
+  'Bosnia and Herzegovina',
+  'Botswana',
+  'Brazil',
+  'Brunei',
+  'Bulgaria',
+  'Burkina Faso',
+  'Burundi',
+  'Cambodia',
+  'Cameroon',
+  'Canada',
+  'Cape Verde',
+  'Central African Republic',
+  'Chad',
+  'Chile',
+  'China',
+  'Colombi',
+  'Comoros',
+  'Congo (Brazzaville)',
+  'Congo',
+  'Costa Rica',
+  "Cote d'Ivoire",
+  'Croatia',
+  'Cuba',
+  'Cyprus',
+  'Czech Republic',
+  'Denmark',
+  'Djibouti',
+  'Dominica',
+  'Dominican Republic',
+  'East Timor (Timor Timur)',
+  'Ecuador',
+  'Egypt',
+  'El Salvador',
+  'Equatorial Guinea',
+  'Eritrea',
+  'Estonia',
+  'Ethiopia',
+  'Fiji',
+  'Finland',
+  'France',
+  'Gabon',
+  'Gambia, The',
+  'Georgia',
+  'Germany',
+  'Ghana',
+  'Greece',
+  'Grenada',
+  'Guatemala',
+  'Guinea',
+  'Guinea-Bissau',
+  'Guyana',
+  'Haiti',
+  'Honduras',
+  'Hungary',
+  'Iceland',
+  'India',
+  'Indonesia',
+  'Iran',
+  'Iraq',
+  'Ireland',
+  'Israel',
+  'Italy',
+  'Jamaica',
+  'Japan',
+  'Jordan',
+  'Kazakhstan',
+  'Kenya',
+  'Kiribati',
+  'Korea, North',
+  'Korea, South',
+  'Kuwait',
+  'Kyrgyzstan',
+  'Laos',
+  'Latvia',
+  'Lebanon',
+  'Lesotho',
+  'Liberia',
+  'Libya',
+  'Liechtenstein',
+  'Lithuania',
+  'Luxembourg',
+  'Macedonia',
+  'Madagascar',
+  'Malawi',
+  'Malaysia',
+  'Maldives',
+  'Mali',
+  'Malta',
+  'Marshall Islands',
+  'Mauritania',
+  'Mauritius',
+  'Mexico',
+  'Micronesia',
+  'Moldova',
+  'Monaco',
+  'Mongolia',
+  'Morocco',
+  'Mozambique',
+  'Myanmar',
+  'Namibia',
+  'Nauru',
+  'Nepal',
+  'Netherlands',
+  'New Zealand',
+  'Nicaragua',
+  'Niger',
+  'Nigeria',
+  'Norway',
+  'Oman',
+  'Pakistan',
+  'Palau',
+  'Panama',
+  'Papua New Guinea',
+  'Paraguay',
+  'Peru',
+  'Philippines',
+  'Poland',
+  'Portugal',
+  'Qatar',
+  'Romania',
+  'Russia',
+  'Rwanda',
+  'Saint Kitts and Nevis',
+  'Saint Lucia',
+  'Saint Vincent',
+  'Samoa',
+  'San Marino',
+  'Sao Tome and Principe',
+  'Saudi Arabia',
+  'Senegal',
+  'Serbia and Montenegro',
+  'Seychelles',
+  'Sierra Leone',
+  'Singapore',
+  'Slovakia',
+  'Slovenia',
+  'Solomon Islands',
+  'Somalia',
+  'South Africa',
+  'Spain',
+  'Sri Lanka',
+  'Sudan',
+  'Suriname',
+  'Swaziland',
+  'Sweden',
+  'Switzerland',
+  'Syria',
+  'Taiwan',
+  'Tajikistan',
+  'Tanzania',
+  'Thailand',
+  'Togo',
+  'Tonga',
+  'Trinidad and Tobago',
+  'Tunisia',
+  'Turkey',
+  'Turkmenistan',
+  'Tuvalu',
+  'Uganda',
+  'Ukraine',
+  'United Arab Emirates',
+  'United Kingdom',
+  'United States',
+  'Uruguay',
+  'Uzbekistan',
+  'Vanuatu',
+  'Vatican City',
+  'Venezuela',
+  'Vietnam',
+  'Yemen',
+  'Zambia',
+  'Zimbabwe',
+];
+mid_index = len(countries) // 2
+mid_countries = countries[mid_index] if len(countries) % 2 != 0 else countries[mid_index-1:mid_index+1]
+# 3. Divide the countries list into two equal lists if it is even if not one more country for the first half.
+half_index = len(countries) // 2
+first_half_countries = countries[:half_index + 1]
+second_half_countries = countries[half_index + 1:]
+# 4. ['China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark'].
+# Unpack the first three countries and the rest as scandic countries.
+countries = [
+    'China', 'Russia', 'USA', 'Finland', 'Sweden', 'Norway', 'Denmark'
+]
+country1, country2, country3, *scandic_countries = countries
+
+# print Results
+print("Sorted Ages:", ages)
+print("Min Age:", min_age)
+print("Max Age:", max_age)
+print("Median Age:", median_age)
+print("Average Age:", average_age)
+print("Age Range:", age_range)
+print("Min - Average Difference:", min_average_diff)
+print("Max - Average Difference:", max_average_diff)
+print("middle Countries", mid_countries)
+print("First Half Countries:", first_half_countries)
+print("Second Half Countries:", second_half_countries)
+print("Unpacked Countries:", country1, country2, country3)
+print("Scandic Countries:", scandic_countries)
